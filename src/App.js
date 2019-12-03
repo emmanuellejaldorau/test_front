27df9549d504de5d79d059433832ab9f0e841f8f
Thomas Victoria <ThomasVictoria@users.noreply.github.com>
2019-12-03 10:17:46 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -31,25 +31,18 @@ function App() {
         <Box>
           <code>
             {`
-              À l’aide de l’API github, vous devrez réaliser une datavisualisation sur les issues du repository microsoft/vscode, à partir d’une base de projet que nous vous aurons fourni.
-
-              Voici les consignes:
-
-              Vous aurez 3 stats à afficher (deux sont des graphiques, nous vous conseillons l’utilisation de la librairie chartjs.org):
-
-              Un graphique en barre vertical permettant de comparer le nombre d’utilisations des labels sur les issues.
-
-              \tUn graphique ligne permettant de suivre l’évolution du nombre d’issues actives dans le temps (Bonus: Ajout de date picker permettant de visualiser les données sur une période sélectionnée).
-
-              \tUne liste des créateurs d’issues sur ce répo classé par nombre d’issues ouvertes (Bonus: barre de recherche permettant de trouver un utilisateur par son nom).
-
-              Chaque point listé au dessus devra être sur un onglet/composant indépendant accessible via une route
-              Comme dit dans le point précédant, l’application devra implémenter un react router.
-
-              Vous aurez carte blanche sur le design du projet ! N'hésitez pas à tout modifier.
-
-              Une fois terminé vous devrez héberger votre réalisation sur un repository github et nous envoyer son lien. Toute fonctionnalité supplémentaire est la bienvenue. Nous vous laissons une semaine pour nous renvoyer le test.
-
+              À l’aide de l’API github, vous devrez réaliser une datavisualisation sur les issues du repository microsoft/vscode, à partir d’une base de projet (https://github.com/Askmona/test_front) que nous vous aurons fourni.
+              Voici la consigne :
+              Vous devrez afficher un graphique ligne permettant de suivre l’évolution du nombre d’issues actives dans le temps (Bonus: Ajout de date picker permettant de visualiser les données sur une période sélectionnée). Pour ce faire vous devrez utiliser la librairie chartjs.org
+                
+              Vous aurez carte blanche sur le design du projet. Une fois terminé vous devrez héberger votre réalisation sur un repository github et nous envoyer son lien. Toute fonctionnalité supplémentaire est la bienvenue. Nous vous laissons une semaine pour nous renvoyer le test.
+                
+              PS: Voici une liste de lien pour chartjs qui vous aideront à appréhender la librairie plus rapidement :
+              https://www.chartjs.org/docs/latest/getting-started/usage.html
+              https://www.chartjs.org/docs/latest/charts/line.html
+              https://www.chartjs.org/docs/latest/axes/cartesian/time.html
+              
+              De plus vous rencontrerez surement des limitations sur l’api github, nous vous invitons à être ingénieux quant à la mise en cache des données en local.
               Bonne chance !
             `}
           </code>
