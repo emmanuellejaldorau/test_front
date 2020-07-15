1eb993013d99026cab5030b54b12ea999427c4c4
Emmanuelle Jaldorau <e.jaldorau@gmail.com>
2020-07-15 07:58:20 +0200

    feat(router): added config

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import styled, { ThemeProvider } from "styled-components";
 import theme from "./theme/";
 import "./theme/baseline.css";
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import Homepage from 'components/Homepage';
+import MuseumDetails from 'components/MuseumDetails';
 
 const Container = styled.div`
   display: flex;
@@ -18,18 +20,23 @@ const Box = styled.div`
   border-radius: 5px;
   background: #e7e7e7;
   white-space: pre-line;
-  padding: 5px;
+  padding: 15px 5px;
 `
 
 function App() {
   return (
+    <BrowserRouter>
     <ThemeProvider theme={theme}>
       <Container>
         <Box>
-          <Homepage />
+          <Switch>
+            <Route exact path="/" component={Homepage} />
+            <Route path="/museums/:museumId" component={MuseumDetails} />
+          </Switch>
         </Box>
       </Container>
-    </ThemeProvider>
+    </ThemeProvider>    
+    </BrowserRouter>
   );
 }
 
