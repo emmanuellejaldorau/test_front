1d449ace36c3b8676a9b9514a9a83328ffcdceda
Emmanuelle Jaldorau <e.jaldorau@gmail.com>
2020-07-15 09:52:40 +0200

    Updates

---
+++ precache-manifest.1e0218c8a3a527075846e7e0ad2f871d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7bd1ad6d1c16fac7f31d55b933b5d7b",
+    "url": "/test_front/index.html"
+  },
+  {
+    "revision": "f2ba3f5e4a3c419c92e2",
+    "url": "/test_front/static/css/main.e1e1910a.chunk.css"
+  },
+  {
+    "revision": "e39d79220154f5a7910b",
+    "url": "/test_front/static/js/2.64e99dab.chunk.js"
+  },
+  {
+    "revision": "b357ab71af3e2e02b0ffa75cb2fade6b",
+    "url": "/test_front/static/js/2.64e99dab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2ba3f5e4a3c419c92e2",
+    "url": "/test_front/static/js/main.69e5f125.chunk.js"
+  },
+  {
+    "revision": "c8ad9ab85e769459a0b2",
+    "url": "/test_front/static/js/runtime-main.949aa6e9.js"
+  }
+]);
