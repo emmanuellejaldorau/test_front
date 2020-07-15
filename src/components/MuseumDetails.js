9cdfe85437d73578a341a33a33eb5afb94858da3
Emmanuelle Jaldorau <e.jaldorau@gmail.com>
2020-07-15 08:08:21 +0200

    feat(MuseumDetails):retrieved data

--- src/components/MuseumDetails.js
+++ src/components/MuseumDetails.js
@@ -1,22 +1,33 @@
-import React, {useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
+import styled from 'styled-components';
 import axios from 'axios';
 
 const MuseumDetails = (props) => {
-    // const [data, setData] = useState(initialData);
-
-    const fetchData = async () =>{
-        const url = 'https://data.culture.gouv.fr/api/records/1.0/search/?dataset=liste-et-localisation-des-musees-de-france&q=&recordid=dfc11f5bcb55f0984706fccfdb7f3d46508df6df';
-        let response = await axios(url)
-        // setData(response.data.records);
-        console.log("happening museum details",response);
-    }
+    const initialData = {nom_du_musee:""}
+    const [data, setData] = useState(initialData);
 
     useEffect(() => {
-        fetchData()
+        const fetchMuseumData = async () =>{
+            const url = 'https://data.culture.gouv.fr/api/records/1.0/search/?dataset=liste-et-localisation-des-musees-de-france&q=&facet=region&facet=departement';
+            let response = await axios(url);
+            if (props.match){
+                const { museumId } = props.match.params;
+                const museum = response.data.records.find(elt => elt.recordid === museumId );
+                console.log("Museum details", museum.fields);
+                setData(museum.fields);
+            }
+        }
+        fetchMuseumData()
     }, []);
+
+    const Box = styled.div`
+        background-color: white;
+    `
     
     return(
-        <div></div>
+        <Box>
+            <h1>{data.nom_du_musee}</h1>
+        </Box>
     )
 }
 
